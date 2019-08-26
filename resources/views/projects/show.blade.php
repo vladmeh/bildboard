bb449e19785c8f90d927bffc29a69ea6c36bf9f1
vladmeh <vladmeh@gmail.com>
2019-08-26 23:57:08 +0300

    Episode_8. Prepping the Frontend.

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,14 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $project->title }}</title>
-</head>
-<body>
-<h1>{{ $project->title }}</h1>
-<div>{{ $project->description }}</div>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{ $project->title }}</h1>
+    <div>{{ $project->description }}</div>
+    <a href="/projects">Go back</a>
+@endsection
