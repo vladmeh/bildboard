dc6e9f18b5096897383cdf0c0682ca1c84b10c2c
vladmeh <vladmeh@gmail.com>
2019-08-28 00:22:57 +0300

    Episode_11. Styling the Project Page.

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,21 +2,16 @@
 
 @section('content')
     <header class="flex items-center mb-3 py-4">
-        <div class="flex justify-between items-center w-full">
-            <h3 class="text-gray-500 text-sm font-normal">My Projects</h3>
-            <a href="/projects/create" class="btn-blue">New Project</a>
+        <div class="flex justify-between items-end w-full">
+            <h2 class="text-gray-500 text-sm font-normal">My Projects</h2>
+            <a href="{{url('/projects/create')}}" class="btn-blue">New Project</a>
         </div>
     </header>
 
     <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse($projects as $project)
             <div class="lg:w-1/3 px-3 pb-6">
-                <div class="bg-white p-5 rounded-lg shadow" style="height: 200px;">
-                    <h3 class="font-normal text-xl py-4 mb-3 -ml-5 border-l-4 border-blue-500 pl-4">
-                        <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
-                    </h3>
-                    <div class="text-gray-500">{{ Illuminate\Support\Str::limit($project->description, 100) }}</div>
-                </div>
+                @include('projects.card')
             </div>
         @empty
             <div>No projects yet.</div>
