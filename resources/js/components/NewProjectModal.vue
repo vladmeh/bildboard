350098dde74f3ae375db7f3a9d77248bb2dc1576
vladmeh <vladmeh@gmail.com>
2019-10-01 00:24:06 +0300

    Episode 42. Object-Oriented JavaScript.

--- resources/js/components/NewProjectModal.vue
+++ resources/js/components/NewProjectModal.vue
@@ -9,25 +9,25 @@
                         <input type="text"
                                id="title"
                                class="block w-full border p-2 text-xs rounded"
-                               :class="errors.title ? 'border-error' : 'border-muted-light'"
+                               :class="form.errors.title ? 'border-error' : 'border-muted-light'"
                                v-model="form.title"
                         >
                         <span class="text-xs italic text-error"
-                              v-if="errors.title"
-                              v-text="errors.title[0]"
+                              v-if="form.errors.title"
+                              v-text="form.errors.title[0]"
                         ></span>
                     </div>
                     <div class="mb-4">
                         <label for="description" class="text-sm block mb-2">Description</label>
                         <textarea id="description"
                                   class="block w-full border p-2 text-xs rounded"
-                                  :class="errors.title ? 'border-error' : 'border-muted-light'"
+                                  :class="form.errors.title ? 'border-error' : 'border-muted-light'"
                                   rows="7"
                                   v-model="form.description"
                         ></textarea>
                         <span class="text-xs italic text-error"
-                              v-if="errors.description"
-                              v-text="errors.description[0]"
+                              v-if="form.errors.description"
+                              v-text="form.errors.description[0]"
                         ></span>
                     </div>
                 </div>
@@ -64,18 +64,18 @@
 </template>
 
 <script>
+    import BirdboardForm from './BirdboardForm.js';
+
     export default {
         data() {
             return {
-                form: {
+                form: new BirdboardForm({
                     title: '',
                     description: '',
                     tasks: [
                         {body: ''},
                     ]
-                },
-
-                errors: {}
+                })
             }
         },
         methods: {
@@ -84,11 +84,12 @@
             },
 
             async submit() {
-                try {
-                    location = (await axios.post('/projects', this.form)).data.message;
-                } catch (error) {
-                    this.errors = error.response.data.errors;
+                if (!this.form.tasks[0].body) {
+                    delete this.form.originalData.tasks;
                 }
+
+                this.form.submit('/projects')
+                    .then(response => location = response.data.message);
             }
         }
     }
